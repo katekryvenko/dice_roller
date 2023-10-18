52f2011db34f8774aa939d9d7da031d8f0e51ef1
Kate Kryvenko <katherinakryvenko@gmail.com>
2023-10-18 14:36:39 +0300

    js code to roll the dice

--- GK2.js
+++ GK2.js
@@ -1,4 +1,20 @@
-function kast(){
+function kast() {
+  let sum = 0;
 
+  for (let i = 1; i <= 6; i++) {
+    const randomValue = Math.floor(Math.random() * 6) + 1;
+    const die = document.getElementById(`terning${i}`);
 
+    // Remove previous side class
+    for (let j = 1; j <= 6; j++) {
+      die.classList.remove(`side${j}`);
+    }
+
+    // Add new side class
+    die.classList.add(`side${randomValue}`);
+
+    sum += randomValue;
+  }
+
+  document.getElementById("sum").textContent = `Sum: ${sum}`;
 }
